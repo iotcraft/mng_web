ebad619cb295612a2312f102317cf8536b28abc4
Senqai <petrboranek@gmail.com>
2023-05-16 13:21:59 +0200

    zkouška php

--- Dobrý weby/mng_web-main/main.php
+++ Dobrý weby/mng_web-main/main.php
@@ -1,51 +1,3 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="vedlejšák.css">
-    <script src="vedlejšák.js"></script>
-    <title>IoT Craft</title>
-</head>
-<body>
-  <div class >
-    <header></header>
-  </div>
-  
-  <div class="wrapper">
-    <!-- Black bar container -->
-  <div class="black-bar">
-    
-    
-    <nav class="navbar">
-      <!-- User label -->
-      <div class="user-label">User</div>
-      <!-- Popup menu -->
-        <div class="popup-menu">
-        <div class="menu-item">Profile</div>
-        <div class="menu-item">Settings</div>
-        <div class="menu-item">Logout</div>
-      </div>
-        <ul>
-        
-          <li><a href="#">Simple</a></li>
-          <li><a href="#">Blockly</a></li>
-          <li><a href="#">Lua</a><bgg/li>
-        
-        </ul>
-      
-    </nav>
-   
-
-    
-  
-  </div>
-  </div>
-
-
-</div>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -110,8 +62,8 @@
           // Display menu options for non-logged-in users
           echo '
           <ul>
-              <li><a href="register.php">Register</a></li>
-              <li><a href="login.php">Login</a></li>
+              <li><a href="register_form.php">Register</a></li>
+              <li><a href="login_form.php">Login</a></li>
           </ul>';
       }
   }
