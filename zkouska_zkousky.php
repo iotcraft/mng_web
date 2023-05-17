c6f4ed84c7243867a27a52dfbca646ba1ab73c0d
mopsik22 <dejv.petrasek@gmail.com>
2023-05-17 12:43:25 +0200

    dejvovo výser

---
+++ zkouska_zkousky.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="zkoska_zkousky.css">
+
+    <title>IoT Craft</title>
+</head>
+<body>
+  <div class >
+    <header></header>
+  </div>
+  
+  <div class="wrapper">
+    <!-- Black bar container -->
+  <div class="black-bar">
+    
+    
+    <nav class="navbar">
+      <!-- User label -->
+      <div class="user-label"><img src="pf.jpg" alt="Avatar" style="width:50px" class="avatar" onclick="toggleMenu()"></div>
+      <div class="sub-menu-wrap" id="subMenu">
+        <div class="sub-menu">
+        <hr>
+   <a href="#" class="menu-item" id="profil">
+      <p>Settings</p>
+    </a>
+    <hr>
+    <a href="#" class="menu-item" >
+      <p>Profile</p>
+    </a>
+    <hr>
+    <a href="login_form.php" class="menu-item" id="logout">
+      <p>Logout</p>
+    </a>
+    <hr>
+        </div>
+      </div>
+      
+ 
+        <ul>
+          <li><a href="Simple.html">Simple</a></li>
+          <li><a href="Začínáme.html">Blockly</a></li>
+          <li><a href="#">Lua</a><bgg/li>
+        </ul>
+      
+    </nav>
+  <a href="https://i.ytimg.com/vi/6SKmcvz1pos/maxresdefault.jpg"></a>
+
+    
+  
+  </div>
+  </div>
+
+
+</div>
+<script>
+let subMenu = document.getElementById("subMenu");
+ function toggleMenu(){
+  subMenu.classList.toggle("open-menu");
+ }
+ 
+</script>
+</body>
+</html>
