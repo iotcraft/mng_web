d48cd9de700bd440bf9776681dc42a160d8e13c0
Senqai <petrboranek@gmail.com>
2023-05-18 14:30:59 +0200

    test

--- Dobrý weby/mng_web-main/blockly.js
+++ Dobrý weby/mng_web-main/blockly.js
@@ -58,7 +58,7 @@ var workspace = Blockly.inject('blocklyDiv', {
 
 // Generate JavaScript code from blocks
 function generateCode() {
-  var code = Blockly.JavaScript.workspaceToCode(workspace);
+  var code = Blockly.Lua.workspaceToCode(workspace);
   return code;
 }
 
